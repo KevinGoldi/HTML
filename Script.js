ba5b20c09bc67a788eb75e095547d10a3c9ae1a2
KevinGoldi <kevin.goldi@gmx.de>
2022-12-18 17:34:43 +0100

    Version 1.0 Voraussichtliche Abgabe der Dateien erfolgen so. Taschenrechner und Animation werden evtl noch überarbeitet.

--- Script.js
+++ Script.js
@@ -1,47 +1,78 @@
-function rechner(a, b, operator) {
-    var operator = document.getElementById("operator");
-    var x = operator.getElementById("operator");
+// function taschenrechner() {
+//     var operator = document.getElementById("operator");
+//     var a = document.getElementById("Zahl1").value;
+//     var b = document.getElementById("Zahl2").value;
+//     var x = document.getElementById("operator").value;
 
-    if (operator == "+") {
-        x = a + b;
-        alert(x);
-    } else if (operator == "-") {
-        x = a - b;
-        alert(x);
-    } else if (operator == "*") {
-        x = a * b;
-        alert(x);
-    } else if (operator == "/") {
-        x = a / b;
-        alert(x);
-    } else if (operator == "%") {
-        x = a % b;
-        alert(x);
-    } else {
-        alert("Bitte für den Operator nur folgende Zeichen benutzen: +, -, *, /, %")
+//     if (operator == "+") {
+//         x = a + b;
+//         alert(x);
+//     } else if (operator == "-") {
+//         x = a - b;
+//         alert(x);
+//     } else if (operator == "*") {
+//         x = a * b;
+//         alert(x);
+//     } else if (operator == "/") {
+//         x = a / b;
+//         alert(x);
+//     } else if (operator == "%") {
+//         x = a % b;
+//         alert(x);
+//     } else {
+//         alert("Bitte für den Operator nur folgende Zeichen benutzen: +, -, *, /, %")
+//     }
+
+//     switch (operator) {
+//         case "+":
+//             x = a + b
+//             alert(x);
+//         case "-":
+//             x = a - b
+//             return x;
+//         case "*":
+//             x = a * b;
+//             return x;
+//         case "/":
+//             x = a / b;
+//             return x;
+//         case "%":
+//             x = a % b;
+//             return x;
+//         default:
+//             alert("Bitte für den Operator nur folgende Zeichen benutzen: +, -, *, /, %")
+//             break;
+//     }
+// }
+
+function rechner(){
+    var operand1 = document.getElementById("Zahl1").value;
+    var operand2 = document.getElementById("Zahl2").value;
+    var operator = document.getElementById("operator").value;
+    var res;
+
+    switch (operator) {
+      case "+" :
+        res = parseInt(operand1) + parseInt(operand2)
+        break;
+      case "-" :
+        res = operand1 - operand2
+        break;
+      case "*" :
+        res = operand1 * operand2
+        break;
+      case "/" :
+        res = operand1 / operand2
+        break;
+      case "%" :
+        res = operand1 % operand2
+        break;
+      default :
+        alert("Bitte überprüfe deine Eingabe nochmals!");
+        break;
     }
-
-    // switch (operator) {
-    //     case "+":
-    //         x = a + b;
-    //         alert(x);
-    //     case "-":
-    //         x = a - b;
-    //         return x;
-    //     case "*":
-    //         x = a * b;
-    //         return x;
-    //     case "/":
-    //         x = a / b;
-    //         return x;
-    //     case "%":
-    //         x = a % b;
-    //         return x;
-    //     default:
-    //         alert("Bitte für den Operator nur folgende Zeichen benutzen: +, -, *, /, %")
-    //         break;
-    // }
-}
+    alert(res);
+  }
 
 function coinFlip() {
     var coin = Math.floor(Math.random() * 2);
